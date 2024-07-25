62e26ebf8d4c47b616522d1e3409c435ff18859b
rishishounak <65448224+rishishounak@users.noreply.github.com>
2024-07-25 16:17:22 +0530

    Update matrixzeros21.cpp

--- matrixzeros21.cpp
+++ matrixzeros21.cpp
@@ -1,54 +1,27 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<int> v1;
-        vector<int> v2;
+        vector<int> rows;
+        vector<int> cols;
+
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[0].size();j++){
                 if(matrix[i][j]==0){
-                    v1.push_back(i);
-                    v2.push_back(j);
+                    rows.push_back(i);
+                    cols.push_back(j);
                 }
             }
         }
-
-
-        for(int i=0;i<v1.size();i++){
+        for(int i=0;i<rows.size();i++){
             for(int j=0;j<matrix[0].size();j++){
-                matrix[i][j]=0;
+                matrix[rows[i]][j]=0;
             }
         }
-        for(int i=0;i<v2.size();i++){
+
+        for(int i=0;i<cols.size();i++){
             for(int j=0;j<matrix.size();j++){
-                matrix[j][i]=0;
+                matrix[j][cols[i]]=0;
             }
         }
     }
 };
-
-int main()
-{
-    Solution s1;
-    vector<vector<int>> matrix1;
-    
-    
-    matrix1={{1,1,1},{1,0,1},{1,1,1}};
-    
-    s1.setZeroes(matrix1);
-    
-    
-    
-    for(int i=0;i<matrix1.size();i++){
-        for(int j=0;j<matrix1[0].size();j++){
-            cout<<matrix1[i][j];
-        }
-        cout<<"\n";
-    }
-
-    return 0;
-}
